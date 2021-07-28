3953869ec7b548c48229a24345919b91e4b26cb4
mdb-vzeddie <68240920+mdb-vzeddie@users.noreply.github.com>
2021-07-28 15:29:28 -0400

    Update RemediateSeriousSAM.ps1
    Added a `NoPrompt` option and uses transcripts to get a more detailed playthrough of what happens during the script run

--- RemediateSeriousSAM.ps1
+++ RemediateSeriousSAM.ps1
@@ -2,33 +2,53 @@
 #Use at your own risk! 
 #Author: Brandon Lee - Virtualizationhowto.com 
 
-$placeholder = "check.txt" 
-$log = "SeriousSAM.log" 
+#   Updated: Vincent Zhen - vincent.zhen@mongodb.com
+
+Param (
+    [switch]$NoPrompt,
+    [string]$LogFile = "SeriousSAM.log"
+)
+
+function Rcv-Output {
+    Param (
+        [string]$ForegroundColor = "White"
+        )
+    process { Write-Host $_ -ForegroundColor $ForegroundColor }
+}
+
+Start-Transcript -Path $LogFile
+Write-Output ""
 try { 
          
-    Write-Host "Checking for SeriousSAM and HiveNightmare vulnerability" -ForegroundColor Yellow 
-    Get-Acl $env:windir\system32\config\SAM | fl | out-file $placeholder 
-    $check = Get-Content $placeholder | Where-Object { $_.Contains("BUILTIN\Users") } 
+    Write-Output "[+] Checking for SeriousSAM and HiveNightmare vulnerability" | Rcv-Output -ForegroundColor Yellow
+    $check = [string]::join(" - ",(((Get-Acl $env:windir\System32\config\SAM).Access | ForEach-Object { '{0}|{1}|{2}' -f $_.IdentityReference,$_.AccessControlType,$_.FileSystemRights }).Split("`n"))) | Where-Object { $_.Contains("BUILTIN\Users") } 
          
     if ($check -ne $null) { 
         try { 
-             
-            Write-Warning "Your Machine is Vulnerable to the SeriousSAM and HiveNightmare vulnerability - Apply permissions changes and delete VSS copies?" -WarningAction Inquire 
-            icacls c:\windows\system32\config\*.* /inheritance:e > $log 
-            vssadmin delete shadows /All /Quiet >> $log 
-            Remove-Item $placeholder 
-            Write-Host "Your computer is now remediated" -ForegroundColor Green 
+            if ($NoPrompt) {
+                Write-Output "[!] Your Machine is Vulnerable to the SeriousSAM and HiveNightmare vulnerability" | Rcv-Output -ForegroundColor Red
+            }
+            else {
+                Write-Warning "[!] Your Machine is Vulnerable to the SeriousSAM and HiveNightmare vulnerability - Apply permissions changes and delete VSS copies?" -WarningAction Inquire 
+            }
+            icacls c:\windows\system32\config\*.* /inheritance:e
+            vssadmin delete shadows /All /Quiet
+            Write-Output "[+] Your computer is now remediated" | Rcv-Output -ForegroundColor Green
         } 
         catch { 
-            Write-Host "You chose not to remediate your host" -ForegroundColor Red 
+            Write-Output "[+] You chose not to remediate your host" -ForegroundColor Red 
         } 
     }     
     else { 
-        Remove-Item $placeholder 
-        Write-Host "Your Machine is not vulnerable to the SeriousSAM or HiveNightmare" -ForegroundColor Green 
+        Write-Output "[+] Your Machine is not vulnerable to the SeriousSAM or HiveNightmare" | Rcv-Output -ForegroundColor Green
     } 
 } 
          
 catch { 
-    Write-Host "Error running the script" -ForegroundColor Red 
+    $ErrMessage = $_.Exception.Message
+    $FailedItem = $_.Exception.ItemName
+    Write-Output "[!] Error running the script: ($FailedItem) $ErrMessage" | Rcv-Output -ForegroundColor Red
 }
+
+Write-Output ""
+Stop-Transcript
